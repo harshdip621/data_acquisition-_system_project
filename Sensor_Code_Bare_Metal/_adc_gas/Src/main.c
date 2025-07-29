4eb2659c4a33d226fad28e1e8a4787a2fc5db5a6
Harshdip Sonekar <harshdipsonekar10@gmail.com>
2025-07-30 02:42:02 +0530

    datasheet

--- Sensor_Code_Bare_Metal/_adc_gas/Src/main.c
+++ Sensor_Code_Bare_Metal/_adc_gas/Src/main.c
@@ -1,3 +1,4 @@
+
 /**
  ******************************************************************************
  * @file           : main.c
@@ -24,38 +25,46 @@
 
 #define RL 10000.0f      // Load resistor in ohms (10kΩ)
 #define VCC 5.0f         // Supply voltage
-#define R0 10000.0f      // Calibrated resistance in clean air 
-#define A 110.47f        // MQ-135 curve constant for CO2 (example)
-#define B -2.862f        // MQ-135 curve exponent for CO2 (example)
+
+#define A 272.4f        // MQ-135 curve constant for CO2 
+#define B -2.718f        // MQ-135 curve exponent for CO2 
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU not initialized. Please initialize the FPU."
 #endif
 
+
+
+
 int main(void) {
     UartInit(9600);
     UartPuts("MQ-135 Gas Sensor\r\n");
     AdcInit();
-
+    uint16_t adcVal = AdcRead();
+    float Vout = ((float)adcVal / 4095.0f) * VCC;
+    if (Vout <= 0.0f) Vout = 0.001f;
+    float Rs = RL * (VCC - Vout) / Vout;
+    float R0=Rs/3.7f;
     while (1) {
         char str[64];
-        uint16_t adcVal = AdcRead();
+         adcVal = AdcRead();
 
-        float Vout = ((float)adcVal / 4095.0f) * VCC;
-        if (Vout <= 0.0f) Vout = 0.001f; 
+         Vout = ((float)adcVal / 4095.0f) * VCC;
+        if (Vout <= 0.0f) Vout = 0.001f;
 
-        float Rs = RL * (VCC - Vout) / Vout;
+         Rs = RL * (VCC - Vout) / Vout;
         float ratio = Rs / R0;
 
-        
+
         float ppm = A * pow(ratio, B);
 
-        sprintf(str, "ADC=%ld, V=%.2fV, Rs=%.1fΩ, CO2=%.1f ppm\r\n", adcVal, Vout, Rs, ppm);
+        sprintf(str, "ADC=%d, V=%.2fV, Rs=%.1fΩ, CO2=%.1f ppm\r\n", adcVal, Vout, Rs, ppm);
         UartPuts(str);
 
         DelayMs(1000);
     }
 }
+
 
 
 
